816a304550c8e0f90d3819b0a86041ff8604e94b
Jose Pinto <zepinto@gmail.com>
2016-11-30 18:58:05 +0000

    Added Actors.

---
+++ src/pt/lsts/imc/actors/IMCActor.java
@@ -0,0 +1,136 @@
+package pt.lsts.imc.actors;
+
+import com.squareup.otto.Subscribe;
+import pt.lsts.imc.msg.Message;
+import pt.lsts.imc.net.IMCNetwork;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.concurrent.Future;
+
+/**
+ * Created by zp on 30-11-2016.
+ */
+public abstract class IMCActor {
+
+    private static final LinkedHashMap<IMCActor, Integer> entities = new LinkedHashMap<>();
+    private static final LinkedHashMap<IMCActor, ArrayList<Class<?>>> inputs = new LinkedHashMap<>();
+    private static final LinkedHashMap<IMCActor, ArrayList<Class<?>>> outputs = new LinkedHashMap<>();
+    private final LinkedHashMap<Class<?>, Boolean> checks = new LinkedHashMap<>();
+
+    public final int id() {
+        synchronized (entities) {
+            if (!entities.containsKey(this)) {
+                entities.put(this, entities.size()+1);
+            }
+            return entities.get(this);
+        }
+    }
+
+    public String name() {
+        Actor ann = getClass().getAnnotation(Actor.class);
+        if (ann == null || ann.name().isEmpty())
+            return getClass().getSimpleName();
+        return ann.name();
+    }
+
+    public final List<Class<?>> outputs() {
+        synchronized (outputs) {
+            if (outputs.containsKey(this)) {
+                return outputs.get(this);
+            }
+        }
+        ArrayList<Class<?>> outgoing = new ArrayList<>();
+        Actor ann = getClass().getAnnotation(Actor.class);
+
+        if (ann == null)
+            outgoing.add(Object.class);
+        else
+            Collections.addAll(outgoing, ann.outputs());
+
+        synchronized (outputs) {
+            outputs.put(this, outgoing);
+        }
+
+        return outgoing;
+    }
+
+    public final List<Class<?>> inputs() {
+        synchronized (inputs) {
+            if (inputs.containsKey(this)) {
+                return inputs.get(this);
+            }
+        }
+        ArrayList<Class<?>> subscriptions = new ArrayList<>();
+
+        for (Method m : getClass().getMethods()) {
+            if ((m.getModifiers() & Modifier.PUBLIC) == 0)
+                continue;
+            if (m.getParameterTypes().length != 1)
+                continue;
+            if (m.getAnnotation(Subscribe.class) == null)
+                continue;
+            subscriptions.add(m.getParameterTypes()[0]);
+        }
+
+        synchronized (inputs) {
+            inputs.put(this, subscriptions);
+        }
+
+        return subscriptions;
+    }
+
+    private void check(Message msg) throws Exception {
+        Boolean admissible;
+        synchronized (checks) {
+            admissible = checks.get(msg.getClass());
+        }
+
+        if (admissible == null) {
+            admissible = false;
+            for (Class<?> c : outputs()) {
+                if (c.isAssignableFrom(msg.getClass())) {
+                    admissible = true;
+                    break;
+                }
+            }
+            synchronized (checks) {
+                checks.put(msg.getClass(), admissible);
+            }
+        }
+
+        if (!admissible) {
+            throw new Exception("Message cannot be sent from this actor");
+        }
+    }
+
+    public final void post(Message msg) throws Exception {
+        check(msg);
+        IMCNetwork.post(msg);
+    }
+
+    public final boolean send(String destination, Message msg) throws Exception {
+        check(msg);
+        try {
+            IMCNetwork.sendUdp(msg, destination);
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void run() {
+        try {
+            IMCNetwork.start();
+            IMCNetwork.register(this);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
