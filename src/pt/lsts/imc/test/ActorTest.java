40bdda16d6aef0bfaeda472d1a4b975711e0bb9e
Jose Pinto <zepinto@gmail.com>
2016-12-05 09:14:56 +0000

    Added message construction example to Actor tests.

--- src/pt/lsts/imc/test/ActorTest.java
+++ src/pt/lsts/imc/test/ActorTest.java
@@ -5,11 +5,17 @@ import com.squareup.otto.Subscribe;
 import pt.lsts.imc.actors.IMCActor;
 import pt.lsts.imc.annotations.Parameter;
 import pt.lsts.imc.annotations.Periodic;
+import pt.lsts.imc.def.SpeedUnits;
+import pt.lsts.imc.def.ZUnits;
 import pt.lsts.imc.msg.EstimatedState;
+import pt.lsts.imc.msg.Loiter;
 import pt.lsts.imc.msg.Message;
+import pt.lsts.imc.msg.PlanControl;
 import pt.lsts.imc.net.IMCQuery;
 import pt.lsts.imc.net.IMCRegistry;
 import pt.lsts.imc.util.PojoConfig;
+
+import java.util.EnumSet;
 
 /**
  * Created by zp on 30-11-2016.
@@ -17,8 +23,12 @@ import pt.lsts.imc.util.PojoConfig;
 public class ActorTest extends IMCActor {
 
 	@Parameter
-	private String name = "CCU-IMC4J";
-	
+	private String sys_name = IMCRegistry.getSysName();
+
+    @Parameter
+    private int sys_id = IMCRegistry.getImcId();
+
+
     @Subscribe
     public void on(Message msg) {
         System.out.printf("%s from %s\n", msg.abbrev(), msg.src());
@@ -31,14 +41,36 @@ public class ActorTest extends IMCActor {
     
     @Override
     public void init() {
-    	IMCRegistry.setSysName(name);
+    	IMCRegistry.setSysName(sys_name);
+        IMCRegistry.setImcId(sys_id);
     }
 
     public static void main(String args[]) throws Exception {
     	
     	if (args.length == 0)
-    		args = new String[] {"--name=My Name", "--x=y"};
+    		args = new String[] {"--sys_name=My Name", "--x=y"};
     	
     	new ActorTest().run(PojoConfig.asProperties(args));
+
+
+        PlanControl m = new PlanControl();
+        m.type = PlanControl.TYPE.PC_REQUEST;
+        m.op = PlanControl.OP.PC_START;
+        m.request_id = 38;
+        m.flags = EnumSet.of(PlanControl.FLAGS.FLG_CALIBRATE);
+
+        m.arg = new Loiter();
+        ((Loiter)m.arg).lat = 60.0;
+        ((Loiter)m.arg).lon = -8.0;
+        ((Loiter)m.arg).radius = 30f;
+        ((Loiter)m.arg).type = Loiter.TYPE.LT_CIRCULAR;
+        ((Loiter)m.arg).z_units = ZUnits.DEPTH;
+        ((Loiter)m.arg).z = 2f;
+        ((Loiter)m.arg).speed = 1.5f;
+        ((Loiter)m.arg).speed_units = SpeedUnits.METERS_PS;
+        m.info = "Start this "+m.arg.abbrev();
+
+        System.out.println(m);
+
     }
 }
--- src/pt/lsts/imc/test/AktorTest.kt
+++ src/pt/lsts/imc/test/AktorTest.kt
@@ -3,30 +3,23 @@ package pt.lsts.imc.test
 import com.squareup.otto.Subscribe
 import pt.lsts.imc.actors.IMCActor
 import pt.lsts.imc.annotations.Periodic
-import pt.lsts.imc.msg.EntityList
-import pt.lsts.imc.msg.EstimatedState
-import pt.lsts.imc.msg.Message
-import pt.lsts.imc.net.IMCNetwork
+import pt.lsts.imc.def.SpeedUnits
+import pt.lsts.imc.def.ZUnits
+import pt.lsts.imc.msg.*
 import pt.lsts.imc.net.IMCQuery
-import pt.lsts.imc.net.IMCRegistry
+import java.util.*
 
-/**
- * Mix-in a method to return the source of a message as String
- * @return The source of this message or <code>unknown</code> if the source hasn't sent an announce yet
- */
-fun Message.srcName(): String = IMCRegistry.resolveSystem(src) ?: "unknown!?"
-
-/**
- * Mix-in a method to return the name of a message
- * @return The name of this message (matches the 'abbrev' in IMC definition)
- */
-fun Message.abbrev() : String = javaClass.simpleName
+fun <T : Message> msg(msg: Class<T>, builder: T.() -> Unit): T {
+    val m = MessageFactory.create(msg.simpleName) as T
+    m.builder()
+    return m;
+}
 
 class KotlinTest : IMCActor() {
 
     @Subscribe
     fun on(msg: Message) {
-        println("${msg.abbrev()} from ${msg.srcName()}")
+        println("${msg.abbrev()} from ${msg.src()}")
     }
 
     @Periodic(3.times(1000))
@@ -37,4 +30,24 @@ class KotlinTest : IMCActor() {
 
 fun main(args : Array<String>) {
     KotlinTest().run()
+
+    val m = msg(PlanControl::class.java) {
+        type = PlanControl.TYPE.PC_REQUEST
+        op = PlanControl.OP.PC_START
+        request_id = 38
+        flags = EnumSet.of(PlanControl.FLAGS.FLG_CALIBRATE)
+        arg = msg(Loiter::class.java) {
+            lat = 60.0
+            lon = -8.0
+            radius = 30f
+            type = Loiter.TYPE.LT_CIRCULAR
+            z_units = ZUnits.DEPTH
+            z = 2f
+            speed = 1.5f
+            speed_units = SpeedUnits.METERS_PS
+        }
+        info = "Start this ${arg.abbrev()}"
+    }
+
+    println (m)
 }
