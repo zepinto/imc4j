51df7bdb55b7bfa5e23f599e9fc0853401a723a7
Jose Pinto <zepinto@gmail.com>
2017-06-26 17:02:54 +0100

    Updated DRIP configurations.

--- dist/drip/drip.sh
+++ dist/drip/drip.sh
@@ -1,10 +1,13 @@
-CONF_FILE=douro.props
-JAVA=/usr/bin/java
+DRIP_HOME=/opt/lsts/drip
+CONF_FILE="$HOME/douro.props"
+JAVA="$DRIP_HOME/jre/bin/java"
 DATE=`date +%Y-%m-%d_%H-%M-%S`
-OUTPUT="log/$DATE.log"
+OUTPUT="$DRIP_HOME/log/$DATE.log"
 mkdir -p log
 rm -f latest.log
 
+cd $DRIP_HOME
 echo "Running with configuration $CONF_FILE..."
 $JAVA -jar Drip.jar $CONF_FILE < /dev/null > $OUTPUT 2>&1 &
 ln -s $OUTPUT latest.log
+
