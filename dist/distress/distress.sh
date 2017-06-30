6e1b60f4f843731f6465fa38661434ac56f15fa9
Paulo Dias <paulo.sousa.dias@gmail.com>
2017-06-30 12:07:59 +0100

    dist/distress/distress.sh: Improving sh script.

--- dist/distress/distress.sh
+++ dist/distress/distress.sh
@@ -1,13 +1,100 @@
-DISTRESS_HOME=/opt/lsts/distress
-CONF_FILE="$DISTRESS_HOME/distress.props"
-JAVA="$DISTRESS_HOME/jre/bin/java"
+#!/bin/sh
+# @author Paulo Dias
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:$PATH
+
+NAME="Distress"
+RUN_HOME="/opt/lsts/distress"
+JARNAME="Distress.jar"
+CONF_FILE="$RUN_HOME/distress.props"
+
+JAVA="/opt/lsts/jre/bin/java"
 DATE=`date +%Y-%m-%d_%H-%M-%S`
-OUTPUT="$DISTRESS_HOME/log/$DATE.log"
-mkdir -p log
-rm -f latest.log
+OUTPUT="$RUN_HOME/log/$DATE.log"
+LATEST="$RUN_HOME/latest.log"
+PIDFILE="$RUN_HOME/pid"
 
-cd $DISTRESS_HOME
-echo "Running with configuration $CONF_FILE..."
-$JAVA -jar Distress.jar $CONF_FILE < /dev/null > $OUTPUT 2>&1 &
-ln -s $OUTPUT latest.log
+start() {
+	echo "Starting $Name"
 
+	checkanyrunning
+	#if [ $? -eq 0 ]; then
+	#	return
+	#fi
+
+	mkdir -p "$RUN_HOME/log"
+	unlink $LATEST
+	cd "$RUN_HOME"
+	echo "Running with configuration $CONF_FILE..."
+	$JAVA -jar $JARNAME $CONF_FILE < /dev/null > $OUTPUT 2>&1 &
+	pid=$!
+  pid=$(ps | grep $JARNAME | grep java | awk '{print $1}' c={1:-1})
+	echo $pid > $PIDFILE
+	echo "PID "$pid
+	ln -s $OUTPUT $LATEST
+}
+
+checkanyrunning() {
+	ps | grep $JARNAME | grep java >/dev/null 2>&1
+	status=$?
+	if [ $status -eq 0 ]; then
+		echo "$NAME is running."
+    echo "Possible PIDs are: " $(ps | grep $JARNAME | grep java | awk '{print $1}' c={1:-1})
+		exit 4
+#	else
+#  echo "$NAME server is not running for PID "$(cat $PIDFILE)"."
+	fi
+#		exit $status
+	return $status
+}
+
+status() {
+	ps | grep java | grep $JARNAME | grep $(cat $PIDFILE 2>/dev/null) >/dev/null 2>&1
+	status=$?
+	if [ $status -eq 0 ]; then
+		echo "$NAME is running for PID "$(cat $PIDFILE)"."
+	else
+		echo "$NAME is not running for PID "$(cat $PIDFILE 2>/dev/null)"."
+    echo "Possible PIDs are: " $(ps | grep $JARNAME | grep java | awk '{print $1}' c={1:-1})
+	fi
+}
+
+stop() {
+		echo "Stopping $Name"
+		ps | grep $JARNAME | grep java | grep $(cat $PIDFILE 2>/dev/null) >/dev/null 2>&1
+		status=$?
+		if [ $status -eq 0 ]; then
+			NPID=$(cat $PIDFILE) && kill -15 $NPID
+			echo "Stopping $Name end"
+		else
+			echo "$NAME is not running for PID "$(cat $PIDFILE 2>/dev/null)"."
+		fi
+		rm -f $PIDFILE
+}
+
+case $1 in
+	tail)
+		status
+		echo "Tailing  $LATEST -> $OUTPUT"
+		tail -f $LATEST
+		;;
+	status)
+		status
+		;;
+	stop)
+		stop
+		;;
+	restart)
+		stop
+		echo "Waiting 5s before running start..."
+		sleep 5
+		start
+		;;
+	start)
+		start
+		;;
+	*)
+		echo "Usage: $0 {start|restart|stop|status|tail}"
+		exit 2
+		;;
+esac
