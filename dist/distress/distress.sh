3bfde7218fdf29e2d2cd1be28535b933edf4e3d0
Paulo Dias <paulo.sousa.dias@gmail.com>
2017-07-02 22:53:08 +0100

    dist/distress/distress.sh: Better stop.

--- dist/distress/distress.sh
+++ dist/distress/distress.sh
@@ -70,6 +70,39 @@ stop()
     rm -f $PIDFILE
 }
 
+stop_all()
+{
+    PIDS=$(ps | grep $JARNAME | grep java | awk '{print $1}' c={1:-1});
+    if [ -z "$PIDS" ]; then
+        echo "$NAME is not running"
+        return
+    fi
+
+    while [ 1 ]; do
+        echo "Stopping $NAME with PIDS $PIDS"
+        kill $PIDS > /dev/null 2>&1
+
+        for r in 0 1 2 3 4 5 6 7 8 9; do
+            PIDS=$(ps | grep $JARNAME | grep java | awk '{print $1}' c={1:-1});
+            if [ -z "$PIDS" ]; then
+                echo "Stopped $NAME"
+                rm -f $PIDFILE
+                return 0
+            else
+                echo "Waiting for process to exit ($r)...  PIDS $PIDS"
+                sleep 1
+            fi
+        done
+
+        PIDS=$(ps | grep $JARNAME | grep java | awk '{print $1}' c={1:-1});
+        echo "Forcing exit...  PIDS $PIDS"
+        kill -9 $PIDS > /dev/null 2>&1
+        sleep 1
+        PIDS=$(ps | grep $JARNAME | grep java | awk '{print $1}' c={1:-1});
+    done
+    rm -f $PIDFILE
+}
+
 cd "$RUN_HOME"
 
 case $1 in
@@ -83,10 +116,10 @@ case $1 in
         status
         ;;
     stop)
-        stop
+        stop_all
         ;;
     restart)
-        stop
+        stop_all
         echo "Waiting 5s before running start..."
         sleep 5
         start
