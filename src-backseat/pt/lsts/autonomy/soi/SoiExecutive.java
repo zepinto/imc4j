8b132949f875c364d6efcc48168917e2fb1cc75a
Jose Pinto <zepinto@gmail.com>
2017-11-23 15:39:48 +0000

    Changed way messages are sent. Now using a queue.

--- src-backseat/pt/lsts/autonomy/soi/SoiExecutive.java
+++ src-backseat/pt/lsts/autonomy/soi/SoiExecutive.java
@@ -8,7 +8,6 @@ import java.lang.reflect.Field;
 import java.util.Date;
 import java.util.EnumSet;
 import java.util.Properties;
-import java.util.concurrent.Future;
 
 import pt.lsts.backseat.TimedFSM;
 import pt.lsts.imc4j.annotations.Consume;
@@ -77,8 +76,9 @@ public class SoiExecutive extends TimedFSM {
 	private PlanSpecification spec = null;
 	private int secs_underwater = 0, count_secs = 0;
 	private int wpt_index = 0;
-	private Future<Void> ongoingIridium = null;
-
+	//private Future<Void> ongoingIridium = null;
+	
+	
 	public SoiExecutive() {
 		setPlanName(soi_plan_id);
 		setDeadline(new Date(System.currentTimeMillis() + mins_timeout * 60 * 1000));
@@ -215,7 +215,7 @@ public class SoiExecutive extends TimedFSM {
 			e.printStackTrace();
 		}
 		// FIXME make sure we wait for reply transmission...
-		ongoingIridium = sendViaIridium(reply, 60);
+		sendViaIridium(reply, 60);
 	}
 
 	@Consume
@@ -231,23 +231,7 @@ public class SoiExecutive extends TimedFSM {
 			}
 		}
 	}
-
-	@SuppressWarnings("unused")
-	private String createSmsReport() {
-		EstimatedState state = get(EstimatedState.class);
-		FuelLevel flevel = get(FuelLevel.class);
-		String plan_chk = "-";
-		if (plan != null)
-			plan_chk = "" + plan.checksum();
-
-		String pos = "?,?";
-		if (state != null) {
-			double[] loc = WGS84Utilities.toLatLonDepth(state);
-			pos = (float) loc[0] + "," + (float) loc[1];
-		}
-		String fuel = flevel == null ? "?" : "" + (int) flevel.value;
-		return remoteSrc + "," + pos + "," + plan_chk + "," + wpt_index + "," + fuel;
-	}
+	
 	
 	private StateReport createStateReport() {
 		EstimatedState estate = get(EstimatedState.class);
@@ -296,8 +280,7 @@ public class SoiExecutive extends TimedFSM {
 		report.stime = (int) (System.currentTimeMillis() / 1000);
 		return report;
 		
-	}
-	
+	}	
 
 	public FSMState init(FollowRefState state) {
 		deadline = new Date(System.currentTimeMillis() + mins_timeout * 60 * 1000);
@@ -317,7 +300,6 @@ public class SoiExecutive extends TimedFSM {
 	}
 
 	public FSMState exec(FollowRefState state) {
-
 		if (plan == null)
 			return this::idleAtSurface;
 
@@ -458,27 +440,15 @@ public class SoiExecutive extends TimedFSM {
 			EnumSet<ReportControl.COMM_INTERFACE> itfs = EnumSet.of(ReportControl.COMM_INTERFACE.CI_GSM);
 			itfs.add(ReportControl.COMM_INTERFACE.CI_SATELLITE);
 			sendReport(itfs);
-			ongoingIridium = sendViaIridium(createStateReport(), seconds - count_secs - 1);			
+			sendViaIridium(createStateReport(), seconds - count_secs - 1);			
 			print("Will wait "+seconds+" seconds");
 		}
 
-		if (ongoingIridium != null && ongoingIridium.isDone()) {
-			try {
-				ongoingIridium.get();
-				print("Report was sent via iridium!");
-			} catch (Exception e) {
-				print("Error transmitting over Iridium: " + e.getMessage());				
-			}
-			ongoingIridium = null;
-		}
-
-		if (count_secs >= seconds / 2 && ongoingIridium == null) {
+		if (count_secs >= seconds) {
 			return this::exec;
-		}
-		else if (count_secs >= seconds) {
-			return this::exec;
-		} else {
-			count_secs++;
+		} 
+		else {
+			count_secs++;			
 			return this::communicate;
 		}
 	}
@@ -490,6 +460,16 @@ public class SoiExecutive extends TimedFSM {
 		setSpeed(speed, SpeedUnits.METERS_PS);
 
 		print("Surfacing...");
+		return this::wait;
+	}
+	
+	public FSMState report_error(FollowRefState ref) {
+		double[] pos = WGS84Utilities.toLatLonDepth(get(EstimatedState.class));
+		setLocation(pos[0], pos[1]);
+		setDepth(0);
+		setSpeed(0, SpeedUnits.METERS_PS);
+
+		print("Surfacing to report error...");
 		return this::wait;
 	}
 
--- src-backseat/pt/lsts/backseat/BackSeatDriver.java
+++ src-backseat/pt/lsts/backseat/BackSeatDriver.java
@@ -3,9 +3,12 @@ package pt.lsts.backseat;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import pt.lsts.imc4j.annotations.Consume;
 import pt.lsts.imc4j.annotations.Periodic;
@@ -32,6 +35,7 @@ import pt.lsts.imc4j.msg.Reference;
 import pt.lsts.imc4j.msg.Reference.FLAGS;
 import pt.lsts.imc4j.msg.SetEntityParameters;
 import pt.lsts.imc4j.msg.TransmissionRequest;
+import pt.lsts.imc4j.msg.TransmissionRequest.COMM_MEAN;
 import pt.lsts.imc4j.msg.TransmissionRequest.DATA_MODE;
 import pt.lsts.imc4j.msg.TransmissionStatus;
 import pt.lsts.imc4j.msg.VehicleMedium;
@@ -45,41 +49,50 @@ public abstract class BackSeatDriver extends TcpClient {
 	protected long startCommandTime = 0;
 	protected boolean paused = true, finished = false;
 	protected String endPlan = null, plan_name = "back_seat";
-	//protected static final String PLAN_NAME = "back_seat";
 	private Reference reference = new Reference();
 	private ExecutorService executor = Executors.newCachedThreadPool();
-		
+	
+	private ConcurrentHashMap<COMM_MEAN, LinkedBlockingDeque<TransmissionRequest>> pendingRequests = new ConcurrentHashMap<>();
+	private ConcurrentHashMap<COMM_MEAN, TransmissionRequest> ongoingRequests = new ConcurrentHashMap<>();
+	
+	{
+		for (COMM_MEAN mean : COMM_MEAN.values()) {
+			pendingRequests.put(mean, new LinkedBlockingDeque<>());
+			//ongoingRequests.put(mean, null);
+		}		
+	}
+	
 	public void setLocation(double latDegs, double lonDegs) {
 		reference.lat = Math.toRadians(latDegs);
 		reference.lon = Math.toRadians(lonDegs);
 		reference.flags.add(FLAGS.FLAG_LOCATION);
 	}
-	
+
 	public void setDepth(double depth) {
 		setZ(depth, ZUnits.DEPTH);
 	}
-	
+
 	public void setAltitude(double alt) {
 		setZ(alt, ZUnits.ALTITUDE);
 	}
-	
+
 	public void setZ(double value, ZUnits units) {
 		DesiredZ z = new DesiredZ();
 		z.value = (float) value;
 		z.z_units = units;
 		reference.z = z;
-		reference.flags.add(FLAGS.FLAG_Z);		
+		reference.flags.add(FLAGS.FLAG_Z);
 	}
-	
+
 	public void setLoiterRadius(double radius) {
-		reference.radius = (float)radius;
+		reference.radius = (float) radius;
 		if (radius != 0)
 			reference.flags.add(FLAGS.FLAG_RADIUS);
 		else
 			reference.flags.remove(FLAGS.FLAG_RADIUS);
-			
+
 	}
-	
+
 	/**
 	 * @return the plan_name
 	 */
@@ -88,7 +101,8 @@ public abstract class BackSeatDriver extends TcpClient {
 	}
 
 	/**
-	 * @param plan_name the plan_name to set
+	 * @param plan_name
+	 *            the plan_name to set
 	 */
 	public final void setPlanName(String plan_name) {
 		this.plan_name = plan_name;
@@ -98,7 +112,7 @@ public abstract class BackSeatDriver extends TcpClient {
 		reference.flags.add(FLAGS.FLAG_MANDONE);
 		finished = true;
 	}
-	
+
 	public void setSpeed(double value, SpeedUnits units) {
 		DesiredSpeed speed = new DesiredSpeed();
 		speed.value = (float) value;
@@ -106,19 +120,18 @@ public abstract class BackSeatDriver extends TcpClient {
 		reference.speed = speed;
 		reference.flags.add(FLAGS.FLAG_SPEED);
 	}
-	
-	
+
 	public boolean arrivedXY() {
 		FollowRefState refState = get(FollowRefState.class);
 		if (refState == null || refState.reference == null)
 			return false;
-		
+
 		if (refState.reference.lat != reference.lat || refState.reference.lon != reference.lon)
 			return false;
-		
+
 		return refState.proximity.contains(FollowRefState.PROXIMITY.PROX_XY_NEAR);
 	}
-	
+
 	public boolean arrivedZ() {
 		FollowRefState refState = get(FollowRefState.class);
 		if (refState == null || refState.reference == null || refState.reference.z == null)
@@ -126,26 +139,24 @@ public abstract class BackSeatDriver extends TcpClient {
 		if (refState.reference.z.z_units != reference.z.z_units || refState.reference.z.value != reference.z.value)
 			return false;
 		return refState.proximity.contains(FollowRefState.PROXIMITY.PROX_Z_NEAR);
-	}	
-	
+	}
+
 	public boolean isUnderwater() {
 		try {
 			return get(VehicleMedium.class).medium == MEDIUM.VM_UNDERWATER;
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			return false;
 		}
 	}
-	
+
 	public boolean hasGps() {
 		try {
 			return get(GpsFix.class).validity.contains(GpsFix.VALIDITY.GFV_VALID_POS);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			return false;
 		}
 	}
- 
+
 	@Consume
 	protected void on(Message msg) {
 		if (msg.src == remoteSrc) {
@@ -154,12 +165,12 @@ public abstract class BackSeatDriver extends TcpClient {
 			}
 		}
 	}
-	
+
 	protected boolean isControlling() {
 		PlanControlState msg = get(PlanControlState.class);
 		return (msg != null && msg.state == STATE.PCS_EXECUTING && msg.plan_id.equals(plan_name));
 	}
-	
+
 	protected boolean isIdle() {
 		PlanControlState msg = get(PlanControlState.class);
 		return (msg != null && msg.state == STATE.PCS_READY);
@@ -177,18 +188,18 @@ public abstract class BackSeatDriver extends TcpClient {
 		return !finished && !paused && isIdle() && (System.currentTimeMillis() - startCommandTime) > 3000
 				&& get(EstimatedState.class) != null;
 	}
-	
+
 	public void setPaused(boolean paused) {
-		this.paused = paused;		
+		this.paused = paused;
 	}
-	
+
 	public void stopExecution() {
 		PlanControl pc = new PlanControl();
 		pc.plan_id = plan_name;
 		pc.op = OP.PC_STOP;
 		pc.type = TYPE.PC_REQUEST;
 		pc.request_id = 679;
-		
+
 		try {
 			send(pc);
 		} catch (Exception e) {
@@ -204,7 +215,7 @@ public abstract class BackSeatDriver extends TcpClient {
 		pc.op = OP.PC_START;
 		pc.type = TYPE.PC_REQUEST;
 		pc.request_id = 679;
-		
+
 		try {
 			send(pc);
 		} catch (Exception e) {
@@ -212,25 +223,25 @@ public abstract class BackSeatDriver extends TcpClient {
 			return;
 		}
 	}
-	
+
 	private void startExecution() {
-		
+
 		double[] pos = WGS84Utilities.toLatLonDepth(get(EstimatedState.class));
 		setLocation(pos[0], pos[1]);
-		
+
 		PlanControl pc = new PlanControl();
 		pc.plan_id = plan_name;
 		pc.op = OP.PC_START;
 		pc.type = TYPE.PC_REQUEST;
 		pc.request_id = 678;
-		
+
 		FollowReference man = new FollowReference();
 		man.control_src = localSrc;
 		man.control_ent = 255;
 		man.loiter_radius = 10;
 		man.timeout = 15;
 		man.altitude_interval = 0.5f;
-		
+
 		PlanManeuver pm = new PlanManeuver();
 		pm.maneuver_id = "1";
 		pm.data = man;
@@ -252,43 +263,39 @@ public abstract class BackSeatDriver extends TcpClient {
 
 	@Periodic(1000)
 	public final void controlLoop() {
-		
+
 		if (finished) {
 			if (endPlan != null) {
-			    print("Starting execution of '"+endPlan+"'.");
+				print("Starting execution of '" + endPlan + "'.");
 				startPlan(endPlan);
-				
+
 				try {
-				    print("Waiting 5s to tidy everything up before exiting...");
-                    Thread.sleep(5000);
-                }
-                catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-			}			
-			disconnect();			
-		}
-		else if (shouldStart()) {
+					print("Waiting 5s to tidy everything up before exiting...");
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			disconnect();
+		} else if (shouldStart()) {
 			startExecution();
-		}
-		else if (isControlling()) {
+		} else if (isControlling()) {
 			if (paused) {
 				stopExecution();
-			}
-			else {
+			} else {
 				FollowRefState curState = get(FollowRefState.class);
 				if (curState == null)
 					return;
 
 				update(curState);
-				
+
 				try {
 					send(reference);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-		}		
+		}
 	}
 
 	@SuppressWarnings("unchecked")
@@ -302,109 +309,111 @@ public abstract class BackSeatDriver extends TcpClient {
 	public abstract void update(FollowRefState fref);
 
 	public BackSeatDriver() {
-	    super();
-	    register(this);		
+		super();
+		register(this);
 	}
 
 	protected void setParam(String entity, String param, String value) {
-	    setParam(entity, new String[] {param, value});
+		setParam(entity, new String[] { param, value });
 	}
 
 	protected void setParam(String entity, String... paramValue) {
-	    if (paramValue == null || paramValue.length < 2) {
-	        print("No parameters to set for entity " + entity);
-	        return;
-	    }
-	    
-	    SetEntityParameters params = new SetEntityParameters();
-	    params.name = entity;
+		if (paramValue == null || paramValue.length < 2) {
+			print("No parameters to set for entity " + entity);
+			return;
+		}
 
-	    for (int i = 1; i < paramValue.length; i = i + 2) {
-	        try {
-	            String param = paramValue[i - 1];
-	            String value = paramValue[i];
-	            EntityParameter p = new EntityParameter();
-	            p.name = param;
-	            p.value = value;
-	            params.params.add(p);
-	        }
-	        catch (Exception e) {
-                print(String.format("Error '%s' setting parameters message to send for %s with %s", e, entity,
-                        paramValue == null || paramValue.length == 0 ? "EMPTY" : Arrays.toString(paramValue)));
-            }
-	    }
+		SetEntityParameters params = new SetEntityParameters();
+		params.name = entity;
 
-	    try {
-	        send(params);
-	    }
-	    catch (Exception e) {
-	        e.printStackTrace();
-	    }
+		for (int i = 1; i < paramValue.length; i = i + 2) {
+			try {
+				String param = paramValue[i - 1];
+				String value = paramValue[i];
+				EntityParameter p = new EntityParameter();
+				p.name = param;
+				p.value = value;
+				params.params.add(p);
+			} catch (Exception e) {
+				print(String.format("Error '%s' setting parameters message to send for %s with %s", e, entity,
+						paramValue == null || paramValue.length == 0 ? "EMPTY" : Arrays.toString(paramValue)));
+			}
+		}
+
+		try {
+			send(params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	protected void activate(String entity) {
-	    setParam(entity, "Active", "true");
+		setParam(entity, "Active", "true");
 	}
 
 	protected void activate(String entity, String... paramValue) {
-        if (paramValue == null || paramValue.length < 2) {
-            setParam(entity, "Active", "true");
-        }
-        else {
-            String[] params = new String[2 + paramValue.length];
-            params[0] = "Active";
-            params[1] = "true";
-            for (int i = 0; i < paramValue.length; i++) {
-                params[i + 2] = paramValue[i];
-            }
-            setParam(entity, paramValue);
-        }
+		if (paramValue == null || paramValue.length < 2) {
+			setParam(entity, "Active", "true");
+		} else {
+			String[] params = new String[2 + paramValue.length];
+			params[0] = "Active";
+			params[1] = "true";
+			for (int i = 0; i < paramValue.length; i++) {
+				params[i + 2] = paramValue[i];
+			}
+			setParam(entity, paramValue);
+		}
 	}
 
 	protected void deactivate(String entity) {
-	    setParam(entity, "Active", "false");
+		setParam(entity, "Active", "false");
 	}
-	
+
 	private LinkedHashMap<Integer, Message> requests_sent = new LinkedHashMap<>();
-	private int request_id = 10000;
-	
+	private AtomicInteger request_id = new AtomicInteger(10000);
+
 	@Consume
 	public void on(TransmissionStatus status) {
 		if (status.src != remoteSrc)
 			return;
-		
+
 		synchronized (requests_sent) {
 			if (requests_sent.containsKey(status.req_id)) {
 				requests_sent.put(status.req_id, status);
 			}
 		}
+		
+		TransmissionRequest ongoing = ongoingRequests.searchValues(1, t -> t.req_id == status.req_id? t : null);
+		if (ongoing != null && status.status != TransmissionStatus.STATUS.TSTAT_IN_PROGRESS) {
+			print("Request "+ongoing.req_id+" has finished: "+status.status);
+			ongoingRequests.remove(ongoing.comm_mean);
+			sendPending();
+		}
 	}
-	
+
 	protected TransmissionRequest createRequest(Message msg, TransmissionRequest.COMM_MEAN mean, int ttl) {
 		TransmissionRequest request = new TransmissionRequest();
 		request.data_mode = DATA_MODE.DMODE_INLINEMSG;
 		request.msg_data = msg;
 		request.comm_mean = mean;
 		request.destination = "broadcast";
-		request.deadline = System.currentTimeMillis()/1000.0 + ttl;
-		request.req_id = ++request_id;
+		request.deadline = System.currentTimeMillis() / 1000.0 + ttl;
+		request.req_id = request_id.incrementAndGet();
 		return request;
 	}
-	
+
 	protected TransmissionRequest createRequest(String text, TransmissionRequest.COMM_MEAN mean, int ttl) {
 		TransmissionRequest request = new TransmissionRequest();
 		request.data_mode = DATA_MODE.DMODE_TEXT;
 		request.txt_data = text;
 		request.comm_mean = mean;
 		request.destination = "broadcast";
-		request.deadline = System.currentTimeMillis()/1000.0 + ttl;
-		request.req_id = ++request_id;
+		request.deadline = System.currentTimeMillis() / 1000.0 + ttl;
+		request.req_id = request_id.incrementAndGet();
 		return request;
 	}
-	
-	protected void sendBlocking(TransmissionRequest request) throws Exception {
-		requests_sent.put(request.req_id, request);
-		send(request);	
+
+	protected void waitFor(TransmissionRequest request) throws Exception {
 		long deadline = (long) (request.deadline * 1000.0);
 		while (System.currentTimeMillis() < deadline) {
 			Thread.sleep(100);
@@ -415,7 +424,7 @@ public abstract class BackSeatDriver extends TcpClient {
 					case TSTAT_DELIVERED:
 					case TSTAT_SENT:
 					case TSTAT_MAYBE_DELIVERED:
-						requests_sent.remove(request.req_id);
+						requests_sent.remove(request.req_id);			
 						return;
 					case TSTAT_INPUT_FAILURE:
 					case TSTAT_TEMPORARY_FAILURE:
@@ -424,45 +433,85 @@ public abstract class BackSeatDriver extends TcpClient {
 						throw new Exception(status.info);
 					default:
 						break;
-					}							
+					}
 				}
 			}
 		}
 		requests_sent.remove(request.req_id);
-		throw new Exception("Transmission timed out.");		
+		throw new Exception("Transmission timed out.");
+	}
+	
+	void sendPending() {
+		for (TransmissionRequest.COMM_MEAN mean : pendingRequests.keySet()) {
+			// is there a transmission that can be sent now?
+			if (!ongoingRequests.containsKey(mean)) {
+				TransmissionRequest req;
+				do {
+					req = pendingRequests.get(mean).pollFirst();
+				}
+				while (req != null && req.deadline < System.currentTimeMillis()/1000.0);
+				
+				if (req != null) {
+					ongoingRequests.put(mean, req);
+					
+					try {
+						send(req);
+						print("Sending "+req.req_id+" request using "+mean);
+						requests_sent.put(req.req_id, req);
+					}							
+					catch (Exception e) {
+						e.printStackTrace();
+					}	
+				}				
+			}
+		}
+	}
+
+	protected int messagesPending() {
+		return ongoingRequests.size() + pendingRequests.values().stream().mapToInt(t -> t.size()).sum();
 	}
 	
 	protected Future<Void> sendVia(Message msg, TransmissionRequest.COMM_MEAN mean, int ttl) {
 		final TransmissionRequest request = createRequest(msg, mean, ttl);
+		pendingRequests.get(mean).add(request);
+		
+		sendPending();
+		
 		return executor.submit(new Callable<Void>() {
+
 			@Override
 			public Void call() throws Exception {
-				sendBlocking(request);
+				waitFor(request);
 				return null;
 			}
 		});
+		
 	}
-	
+
 	protected Future<Void> sendVia(String text, TransmissionRequest.COMM_MEAN mean, int ttl) {
 		final TransmissionRequest request = createRequest(text, mean, ttl);
+		pendingRequests.get(mean).add(request);
+
+		sendPending();
+				
 		return executor.submit(new Callable<Void>() {
 			@Override
 			public Void call() throws Exception {
-				sendBlocking(request);
+				waitFor(request);
 				return null;
 			}
 		});
 	}
 	
 	protected Future<Void> sendViaIridium(Message msg, int ttl) {
-		return sendVia(msg, TransmissionRequest.COMM_MEAN.CMEAN_SATELLITE, ttl);		
+		return sendVia(msg, TransmissionRequest.COMM_MEAN.CMEAN_SATELLITE, ttl);
 	}
-	
+
 	protected Future<Void> sendViaIridium(String text, int ttl) {
-		return sendVia(text, TransmissionRequest.COMM_MEAN.CMEAN_SATELLITE, ttl);		
+		return sendVia(text, TransmissionRequest.COMM_MEAN.CMEAN_SATELLITE, ttl);
 	}
-	
+
 	protected Future<Void> sendViaSms(String text, int ttl) {
-		return sendVia(text, TransmissionRequest.COMM_MEAN.CMEAN_GSM, ttl);		
+		return sendVia(text, TransmissionRequest.COMM_MEAN.CMEAN_GSM, ttl);
 	}
 }
