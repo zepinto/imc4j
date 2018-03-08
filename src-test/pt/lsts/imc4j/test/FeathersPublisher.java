e1e87bd5a1ed02216b206bf3cebfbd42e3aaa90b
zp <zp@zp-lsts>
2018-03-08 19:42:26 +0000

    Also publishing to Ripples.

--- src-test/pt/lsts/imc4j/test/FeathersPublisher.java
+++ src-test/pt/lsts/imc4j/test/FeathersPublisher.java
@@ -1,15 +1,14 @@
 package pt.lsts.imc4j.test;
 
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Logger;
 
-import javax.xml.bind.JAXB;
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 
-import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonArray;
 import com.eclipsesource.json.JsonObject;
 
@@ -23,47 +22,47 @@ import pt.lsts.imc4j.util.WGS84Utilities;
 
 public class FeathersPublisher extends AbstractActor {
 
-	
 	@Parameter
-	String ripplesUrl = "http://ripples.lsts.pt/positions";
-	
+	String ripplesUrl = "http://ripples.lsts.pt/api/v1/systems";
+
 	@Parameter
 	double latitude = 41;
-	
+
 	@Parameter
 	double longitude = -8;
-	
+
 	@Parameter
 	double xSpeed = 0.5;
-	
+
 	@Parameter
 	double ySpeed = -0.5;
-	
+
 	@Parameter
 	String sysName = "feather-01";
-	
+
 	@Parameter
 	int imcId = 20045;
-	
+
 	@Parameter
 	String sensorClass = "Drifter";
-	
-	double lastUpdate;
-	private static final SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-	
+
+	double lastUpdate = System.currentTimeMillis();
+	private static final SimpleDateFormat fmt = new SimpleDateFormat("YYYY-MM-dd'T'HH:mm:ssZ");
+
 	public FeathersPublisher(ActorContext context) throws JAXBException {
 		super(context);
 	}
-	
+
 	@Periodic(1000)
 	public void updatePosition() {
 		double deltaT = (System.currentTimeMillis() - lastUpdate) / 1000.0;
 		double[] newPos = WGS84Utilities.WGS84displace(latitude, longitude, 0, xSpeed * deltaT, ySpeed * deltaT, 0);
 		latitude = newPos[0];
 		longitude = newPos[1];
-		Logger.getLogger(getClass().getSimpleName()).info(latitude+", "+longitude);
+		Logger.getLogger(getClass().getSimpleName()).info(latitude + ", " + longitude);
+		lastUpdate = System.currentTimeMillis();
 	}
-	
+
 	@Periodic(5_000)
 	@Publish(RemoteSensorInfo.class)
 	public void publishToNeptus() {
@@ -74,17 +73,16 @@ public class FeathersPublisher extends AbstractActor {
 		info.sensor_class = sensorClass;
 		info.id = sysName;
 		try {
-			Logger.getLogger(getClass().getSimpleName()).info("Posting "+info);
+			Logger.getLogger(getClass().getSimpleName()).info("Posting " + info);
 			send(info);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}	
-	
+	}
+
 	@Periodic(10_000)
-	public void publishToRipples() {
-		
+	public void publishToRipples() throws Exception {
+
 		JsonObject obj = new JsonObject();
 		obj.add("imcid", imcId);
 		obj.add("name", sysName);
@@ -92,9 +90,20 @@ public class FeathersPublisher extends AbstractActor {
 		obj.add("iridium", "");
 		obj.add("created_at", fmt.format(new Date()));
 		obj.add("updated_at", fmt.format(new Date()));
-		System.out.println(obj.toString());
+
+		URL url = new URL(ripplesUrl);
+		HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+		httpCon.setDoOutput(true);
+		httpCon.setRequestMethod("PUT");
+		httpCon.setRequestProperty("Content-Type", "application/json");
+		OutputStreamWriter out = new OutputStreamWriter(httpCon.getOutputStream());
+		obj.writeTo(out);
+		out.close();
+		httpCon.getInputStream();
+
+		Logger.getLogger(getClass().getSimpleName()).info("Response: "+httpCon.getResponseMessage());
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		AbstractActor.exec(FeathersPublisher.class);
 	}
